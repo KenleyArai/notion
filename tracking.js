909864115b5ccac90bfdedaeb6890a54a5c0fae4
Kenley Arai <me@kenleyar.ai>
2021-06-25 13:00:23 -0700

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -1,13 +1,1 @@
-document.getElementsByClassName("notion-block-dd0e10c48ebd467680a861b6e5930f9e")[0].onclick = fathom.trackGoal('W63LODKZ', 0);
-document.getElementsByClassName("notion-block-ea18b26944f14c839ae7c7f7e3881658")[0].onclick = fathom.trackGoal('IGVZBVUE', 0);
-document.getElementsByClassName("notion-block-bf5788b18ea447cead4861d88baff799")[0].onclick = fathom.trackGoal('IOZBZK9O', 0);
-document.getElementsByClassName("notion-block-3ec68c05aa9c40bbb3941d6fee03cf8a")[0].onclick = fathom.trackGoal('WX7QBP5H', 0);
-document.getElementsByClassName("notion-block-bb83df928dd84885ad28a3c47c97f057")[0].onclick = fathom.trackGoal('NZSCMVXI', 0);
-document.getElementsByClassName("notion-block-5037072a8b694791b06094da17db5075")[0].onclick = fathom.trackGoal('0K8YOOYY', 0);
-
-
-Array.from(document.getElementsByClassName("notion-collection-card notion-collection-card-size-small")).map( 
-      obj => obj.onclick = window.fathom.trackGoal('QVKYUVGL', 0)
-  );
-
-heap.identify('guest');
+document.getElementsByClassName("notion-block-1a332b32b62f4c51b0760ccd407d9a56")[0].onclick = fathom.trackGoal('XZCFTIMS', 0);
