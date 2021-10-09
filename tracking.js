b0aa1db7bcfffe42a5422e50593bf3b487ad52ba
Kenley Arai <me@kenleyar.ai>
2021-10-09 12:12:17 -0700

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -1,1 +1,4 @@
-document.getElementsByClassName("notion-block-1a332b32b62f4c51b0760ccd407d9a56")[0].onclick = fathom.trackGoal('XZCFTIMS', 0);
+document.getElementById("block-1a332b32b62f4c51b0760ccd407d9a56").onclick = fathom.trackGoal('XZCFTIMS', 0);
+document.getElementById("block-a0cdfb4788844ed7a3cc7b0db8da85ac").onclick = fathom.trackGoal('INK8QSCU', 0);
+document.getElementById("block-940f5388078341a3962abf2a20c9f668").onclick = fathom.trackGoal('GJVHAIXR', 0);
+document.getElementById("block-9214cc6549e5406d81f2baa93cbbc8d0").onclick = fathom.trackGoal('4RPO67CP', 0);
